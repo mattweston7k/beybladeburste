99286a620356a78431f775026b56eacacba7d18b
beybladeburst <no@no.no>
2021-03-04 14:34:36 +0800

    67.16

--- include/common.js
+++ include/common.js
@@ -7,8 +7,8 @@ const L = func => window.addEventListener('DOMContentLoaded', func);
 const count = el => document.querySelectorAll(el).length;
 const query = window.location.search.substring(1).split('&').map(q => q.split('=')).reduce((obj, [p, v]) => ({...obj, [p]: v}), {});
 const groups = [
-    ['layer7a', 'layer7b', 'layer7c', 'layer6s', 'layer6r', 'LB', 'layer6c', 'layer5b', 'layer5c',
-        'remake', 'layer5w', 'layer5', 'layer4', 'layer3', 'layer2', 'layer1'],
+    ['layer7a', 'layer7b', 'layer7c'], ['layer6s', 'layer6r', 'LB', 'layer6c'], ['layer5b', 'layer5c', 'layer5w'],
+    ['remake', 'layer4', 'layer3', 'layer2', 'layer1'],
     ['disk3', 'disk2', 'frame', 'disk1'],
     ['metal', 'dash', 'high', 'driver4', 'driver3', 'driver2', 'driver1'], ['other']
 ];
