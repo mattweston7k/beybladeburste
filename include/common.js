3e5d4f41950d70c881e6c0d861f3e5f675774312
beybladeburst <no@no.no>
2021-03-06 13:50:16 +0800

    69.18

--- include/common.js
+++ include/common.js
@@ -1,11 +1,11 @@
-document.head.insertAdjacentHTML('afterbegin', `<style>
+setTimeout(() => document.head.insertAdjacentHTML('afterbegin', `<style>
 html {background:black;}
 html::before {
     content:'請嘗試更新你的瀏覽器，或使用 Chrome 或 Safari';
     text-align:center;color:white;font-size:4em;
     position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);
 }
-body {opacity:0;transition:opacity .5s;}</style>`);
+body {opacity:0;transition:opacity .5s;}</style>`), 1000);
 
 navigator.serviceWorker.register('/worker.js').then(async () => {
     const html = await (await caches.match('/include/head.html') || await fetch('/include/head.html')).text();
